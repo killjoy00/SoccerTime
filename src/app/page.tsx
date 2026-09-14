3282bf922c236b7b5ba8b268588dcb1f4257bfec
killjoy00 <killjoy00@yahoo.com>
2026-09-14 14:50:46 -0500

    Use integrated app navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
 import SoccerTimeClient from "./SoccerTimeClient";
 
 export default function Page(){
-  return <>
-    <SoccerTimeClient/>
-    <Link className="historyShortcut" href="/history" aria-label="Open rivalry history and records">📊 History</Link>
-  </>;
+  return <SoccerTimeClient/>;
 }
